23db42d3769f31934af22038fdf638f7a768d243
Vicente Bertolotti <bertolotti2008@gmail.com>
2025-09-09 17:29:46 -0400

    Worked on the UI for the Contact Manager.

--- renderer/src/views/ManagerView.vue
+++ renderer/src/views/ManagerView.vue
@@ -1,22 +1,22 @@
 
 <template>
-    <div class="flex flex-col py-2 px-4">
-    <h2 class="text-3xl font-bold mb-4">Contact Manager</h2>
-    <div class="flex">
-        <form action="" class="space-y-3">
-            <div class="flex flex-col space-y-3">
-                <div class="flex space-x-2">
-                    <div class="border-2 border-github-gray-border text-github-gray-text flex justify-center items-center px-1 rounded-sm">To:</div>
-                    <input type="email" placeholder="theiremail@bussiness.com" v-model="mailForm.to">
+    <div class="flex flex-col py-2 px-12 w-full">
+        <h2 class="text-3xl font-bold my-6">Contact Manager</h2>
+        <div class="flex">
+            <form action="" class="space-y-3 w-full">
+                <div class="flex flex-col space-y-3 w-full">
+                    <div class="flex space-x-2 w-full mb-6">
+                        <div class="border-2 border-github-gray-border text-github-gray-text flex justify-center items-center px-1 rounded-sm">To:</div>
+                        <input type="email" placeholder="companyemail@bussiness.com" v-model="mailForm.to" class="w-full">
+                    </div>
+                    <input class="border-t-0 border-x-0 bg-github-background border-b-github-gray-border border-b-2 rounded-none focus:border-0 focus:border-b-2" type="text" placeholder="Subject" v-model="mailForm.subject">
+                    <textarea name="" id="" placeholder="Type / to insert files and more" class="block border-transparent h-72 bg-github-background" v-model="mailForm.text"></textarea>
                 </div>
-                <input class="border-t-0 border-x-0 bg-github-background border-b-github-gray-border border-b-2 rounded-none focus:border-0 focus:border-b-2" type="text" placeholder="Subject" v-model="mailForm.subject">
-                <textarea name="" id="" placeholder="Type / to insert files and more" class="block border-none bg-github-background" v-model="mailForm.text"></textarea>
-            </div>
-            <button class="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-                Send
-            </button>
-        </form>
-    </div>
+                <button class="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
+                    Send
+                </button>
+            </form>
+        </div>
     </div>
 </template>
 
